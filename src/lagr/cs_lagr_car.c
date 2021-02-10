fa6c81766bd983ace7be6aa7f73322a8e2671222
Panos Asproulis <panos.asproulis@renuda.com>
2021-02-10 19:05:40 +0100

    Minor modifications

--- src/lagr/cs_lagr_car.c
+++ src/lagr/cs_lagr_car.c
@@ -266,7 +266,6 @@ cs_lagr_car(int              iprev,
     BFT_MALLOC(dissip, ncel, cs_real_t);
 
     if (extra->itytur == 2 || extra->itytur == 4 || extra->iturb == 50) {
-
       for (cs_lnum_t cell_id = 0; cell_id < ncel; cell_id++) {
         energi[cell_id] = extra->cvar_k->vals[iprev][cell_id];
         dissip[cell_id] = extra->cvar_ep->vals[iprev][cell_id];
